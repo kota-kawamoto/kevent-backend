c119f78445b0705a66cae8a07ac8e5b9a5f83642
kawamoto <krt34kk0@gmail.com>
2025-06-22 10:37:03 +0900

    #11 重複削除

--- src/packages/Controllers/Controllers/Users/FindUserController.php
+++ src/packages/Controllers/Controllers/Users/FindUserController.php
@@ -2,34 +2,6 @@
 
 namespace packages\Controllers\Controllers\Users;
 
-use Illuminate\Http\JsonResponse;
-use packages\Presenter\Resources\Users\FindUserResource;
-use packages\UseCase\Interfaces\Users\FindUserUseCaseInterface;
-
-/**
- * ユーザ詳細 Controller クラス
- * @package packages\Controllers\Controllers\Users
- */
-class FindUserController
-{
-    public function __construct(private FindUserUseCaseInterface $useCase) {}
-
-    /**
-     * @param integer $id
-     * @return JsonResponse
-     */
-    public function __invoke(int $id): JsonResponse
-    {
-        return response()->json(
-            new FindUserResource($this->useCase->handle($id))
-        );
-    }
-}
-
-<?php
-
-namespace packages\Controllers\Controllers\Users;
-
 // use Illuminate\Container\Attributes\Log;
 use Illuminate\Http\JsonResponse;
 use packages\Presenter\Resources\Users\FindUserResource;
